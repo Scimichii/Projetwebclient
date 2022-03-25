387693f397f3a3ffe88c21fa1b90d3ef880f6c1b
alex <22001926@campus.unicaen.fr>
2022-03-25 15:48:53 +0100

    partie b

--- emberCTP/ember-2022-1-td6/app/routes/order.js
+++ emberCTP/ember-2022-1-td6/app/routes/order.js
@@ -1,3 +1,12 @@
 import Route from '@ember/routing/route';
+import Abstractroute from "./AbstractRoute";
 
-export default class OrderRoute extends Route {}
+export default class OrderRoute extends Abstractroute {
+
+  model(param) {
+    console.log(param);
+    return this.store.findRecord('order', param.order_id, {
+      include: 'orderdetails.product,user',
+    });
+  }
+}
